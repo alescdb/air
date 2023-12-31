427ea5c587686f478a2663e5747d3f377e59469d
Alexandre Del Bigio <alex@cosinus.org>
2023-12-31 14:32:39 +0100

    prompt template

--- src/llama.rs
+++ src/llama.rs
@@ -1,14 +1,16 @@
+use crate::{ichat::IChat, verbose};
 use async_trait::async_trait;
 use llama_cpp_rs::{
     options::{ModelOptions, PredictOptions},
     LLama,
 };
 use openai::chat::ChatCompletionMessage;
-use crate::ichat::IChat;
+use crate::logs::*;
 
 pub struct LLamaChat {
     pub model: String,
     pub system: Option<String>,
+    pub prompt_template: Option<String>,
 }
 
 #[async_trait]
@@ -25,35 +27,52 @@ impl IChat for LLamaChat {
         // none
     }
 
-    async fn chat(&mut self, prompt: String, _history: Option<Vec<ChatCompletionMessage>>) -> String {
+    async fn chat(
+        &mut self,
+        prompt: String,
+        _history: Option<Vec<ChatCompletionMessage>>,
+    ) -> String {
         let model_options: ModelOptions = ModelOptions::default();
 
-        let llama = LLama::new(
-            self.model.clone(),
-            &model_options,
-        ).unwrap();
+        let llama: LLama = LLama::new(self.model.clone(), &model_options).unwrap();
 
-        let predict_options = PredictOptions {
+        let predict_options: PredictOptions = PredictOptions {
             debug_mode: false,
             temperature: 0.2,
             tokens: 512,
             ..Default::default()
         };
-
-        let results = llama.predict(
-            prompt.into(),
-            predict_options,
-        ).unwrap();
+        let prompt_fmt = self.get_prompt(prompt);
+        verbose!("Prompt Template : {}", prompt_fmt);
+        let results: String = llama.predict(prompt_fmt.into(), predict_options).unwrap();
 
         return results;
     }
 }
 
 impl LLamaChat {
-    pub fn new(model: String) -> Self {
+    fn get_prompt(&self, prompt: String) -> String {
+        if self.prompt_template.is_none() {
+            return prompt;
+        }
+
+        let mut sys = "".to_string();
+        if self.system.is_some() {
+            sys = self.system.clone().unwrap();
+        }
+        return self
+            .prompt_template
+            .clone()
+            .unwrap()
+            .replace("{system}", &sys)
+            .replace("{prompt}", &prompt)
+            .to_string();
+    }
+    pub fn new(model: String, prompt_template: Option<String>) -> Self {
         return LLamaChat {
             model,
             system: None,
+            prompt_template,
         };
     }
 }
