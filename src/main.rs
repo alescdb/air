f15ac553d0622965ef6f783f42062cdcf748eb4f
Alexandre Del Bigio <alex@cosinus.org>
2023-12-31 13:13:37 +0100

    selectable local model

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,22 @@
 mod chatgpt;
 mod config;
-mod options;
 mod history;
-mod path;
-mod logs;
-mod llama;
 mod ichat;
+mod llama;
+mod logs;
+mod options;
+mod path;
 
-use crate::logs::*;
-use termimad::*;
 use crate::chatgpt::ChatGPT;
 use crate::config::{display_setup, load_setup, Setup};
-use crate::options::{display_options, parse_command_line};
 use crate::history::History;
-use termimad::crossterm::style::Stylize;
-use crate::ichat::{IChat};
+use crate::ichat::IChat;
 use crate::llama::LLamaChat;
-
+use crate::logs::*;
+use crate::options::{display_options, parse_command_line};
+use config::LLamaSetup;
+use termimad::crossterm::style::Stylize;
+use termimad::*;
 
 fn display(markdown: bool, content: String) {
     if markdown {
@@ -27,10 +27,24 @@ fn display(markdown: bool, content: String) {
     }
 }
 
+fn get_local<'a>(local: &'a [LLamaSetup], name: &str) -> Option<&'a LLamaSetup> {
+    for l in local {
+        if l.name.eq(name) {
+            return Some(l);
+        }
+    }
+    return None;
+}
 
-fn get_chat(setup: &Setup) -> Box<dyn IChat> {
-    if setup.llama.enable {
-        return Box::new(LLamaChat::new(setup.llama.model.clone()));
+fn get_chat(local: &Option<String>, setup: &Setup) -> Box<dyn IChat> {
+    if !local.is_none() {
+        let llama = get_local(&setup.local, &local.clone().unwrap());
+        if llama.is_some() {
+            return Box::new(LLamaChat::new(llama.unwrap().model.clone()));
+        } else {
+            error!("Can't find local model name in setup : '{}'", local.clone().unwrap());
+            std::process::exit(10);
+        }
     }
     return Box::new(ChatGPT::new(setup.apikey.clone()));
 }
@@ -46,15 +60,18 @@ async fn main() -> Result<(), Error> {
     };
     let options = parse_command_line(setup.markdown);
     let mut history = History::new(setup.expiration);
-    let mut chatgpt = get_chat(&setup);
+    let mut chatgpt = get_chat(&options.local, &setup);
 
     unsafe {
-        VERBOSE = if cfg!(debug_assertions) { true } else { options.verbose };
+        VERBOSE = if cfg!(debug_assertions) {
+            true
+        } else {
+            options.verbose
+        };
     }
     verbose!("Configuration Path : {}", path::get_config_directory());
     verbose!("History File : {:?}", path::get_config_path("history.json"));
     verbose!("Setup File : {:?}", path::get_config_path("setup.json"));
-
 
     unsafe {
         if VERBOSE {
@@ -84,7 +101,9 @@ async fn main() -> Result<(), Error> {
         std::process::exit(5);
     }
 
-    let answer = chatgpt.chat(options.prompt.to_string(), Some(history.get_completions())).await;
+    let answer = chatgpt
+        .chat(options.prompt.to_string(), Some(history.get_completions()))
+        .await;
     display(options.markdown, answer.clone());
 
     history.add(chatgpt.get_name(), &*options.prompt, &*answer.clone());
