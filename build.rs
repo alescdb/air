8f0000b0f1869a42ab8be75a9c9396391fc67102
Alexandre Del Bigio <alex@cosinus.org>
2024-02-04 18:56:26 +0100

    cuda support

--- build.rs
+++ build.rs
@@ -8,6 +8,6 @@ fn main() {
         println!("cargo:rerun-if-env-changed=CUDA_PATH");
         println!("cargo:rustc-cfg=feature=\"cuda\"");
     } else {
-        println!("cargo:note=CUDA_PATH not found. If you want CUDA support, please define CUDA_PATH and enable the 'cuda_support' feature.");
+        println!("cargo:note=CUDA_PATH not found. If you want CUDA support, please install nvidia cuda");
     }
 }
