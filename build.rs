f8cedfda39b4b243bd65e54a68296fb64453543f
Alexandre Del Bigio <alex@cosinus.org>
2024-02-04 19:01:45 +0100

    cuda feature

--- build.rs
+++ build.rs
@@ -4,7 +4,7 @@ fn main() {
     let cuda_path_defined = env::var("CUDA_PATH").is_ok();
 
     if cuda_path_defined {
-        println!("cargo:warning=CUDA_PATH found, enableling CUDA support");
+        println!("cargo:warning=CUDA_PATH found, enabling CUDA support");
         println!("cargo:rerun-if-env-changed=CUDA_PATH");
         println!("cargo:rustc-cfg=feature=\"cuda\"");
     } else {
